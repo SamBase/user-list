25a5a183a5d66494c75217994ad2e8f01e714a92
Shubham Singh <s.bb.singh@accenture.com>
2026-01-21 21:27:55 +0530

    Add users cards

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ async function fetchUserList(){
 
         if(response.ok){
             const jsonResponse = await response.json();
-            console.log("typeof jsonResponse"+typeof jsonResponse);
-            console.log("Array.isArray(jsonResponse)"+Array.isArray(jsonResponse));
+            return jsonResponse;
         }else{
             throw new Error("Failed to fetch users!");
         }
@@ -14,4 +13,63 @@ async function fetchUserList(){
     }
 }
 
-fetchUserList();
+async function displayUsers(userList){
+    console.log("displayUsers --START");
+    
+    try{
+        const userCardsHtml = document.querySelector(".userCards");
+        // const userCardsHtml = document.getElementById("xyz");
+
+        console.log("userCardsHtml:"+userCardsHtml.outerHTML);
+        
+
+        let userListCards= userList.map((element)=>createUserDetailCard(element));
+        console.log("--------"+userListCards);
+
+        for (let index = 0; index < userListCards.length; index++) {
+            userCardsHtml.appendChild(userListCards[index]);
+        }
+
+    }catch(error){
+        console.error(error);
+    }
+    console.log("displayUsers --END");
+}
+
+function createUserDetailCard(user){
+    try{
+        let cardDiv = document.createElement('div');
+        cardDiv.classList.add("userCard");
+
+        let userNamDiv = document.createElement('div');
+        userNamDiv.classList.add("userName");
+        userNamDiv.textContent = user.name;
+
+        let userEmailDiv = document.createElement('div');
+        userEmailDiv.classList.add("userEmail");
+        userEmailDiv.textContent = user.email;
+
+        let userCompanyDiv = document.createElement('div');
+        userCompanyDiv.classList.add("userCompany");
+        userCompanyDiv.textContent = user.company.name;
+
+        cardDiv.appendChild(userNamDiv);
+        cardDiv.appendChild(userEmailDiv);
+        cardDiv.appendChild(userCompanyDiv);
+
+        console.log(cardDiv.outerHTML);
+        
+
+        return cardDiv;
+
+    }catch(error){
+        console.error(error);
+    }
+}
+
+async function f1(){
+    const userList = await fetchUserList();
+    displayUsers(userList);
+}
+
+f1();
