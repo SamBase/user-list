245800577b6c67c0847c819d03163392a5bcb764
Shubham Singh <s.bb.singh@accenture.com>
2026-01-22 11:20:34 +0530

    Refactor code

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let userList = null;
+let isUserListFetched = false;
 const userCardsHtml = document.getElementById("userCards");
 
 const searchBoxHtml = document.getElementById("searchBox");
@@ -7,6 +8,7 @@ searchBoxHtml.addEventListener('input', (event) => { search(event); });
 function search(event) {
     let nameToSearch = event.target.value;
 
+    //if searchbox is empty then display all the users
     if (nameToSearch.trim() == "") {
         main();
         return null;
@@ -26,115 +28,83 @@ function search(event) {
 
 function searchUserName(nameToSearch) {
     console.log("searching for name:" + nameToSearch);
-    try {
-        console.log(userList)
-        let result = userList.find((element) => {
-            console.log(element);
-            return element.name == nameToSearch
-        });
+    let result = userList.find((element) => {
+        return element.name == nameToSearch
+    });
 
-        console.log("Search ended:" + result);
+    console.log("Search result:" + result);
 
 
-        if (result != null) {
-            return result;
-        } else {
-            return null;
-        }
-
-    } catch (error) {
-        console.error(error);
+    if (result != null) {
+        return result;
+    } else {
+        return null;
     }
 }
 
 async function fetchUserList() {
+    if (isUserListFetched == true) return userList;
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
 
         if (response.ok) {
             const jsonResponse = await response.json();
+            isUserListFetched = true;
+            console.log("User details fetched succesfully");
+            console.log("Total users:" + jsonResponse.length);
+            console.log(jsonResponse);
+
             return jsonResponse;
         } else {
             throw new Error("Failed to fetch users!");
         }
     } catch (error) {
-        console.error(error);
+        console.error("Error occured while fetching user list:" + error);
     }
 }
 
-async function displayUserCards(userDetailCards) {
+function displayUserCards(userDetailCards) {
+    resetUserCardsWindow();
 
-    try {
-        resetUserCardsWindow();
-
-        for (let index = 0; index < userDetailCards.length; index++) {
-            displayUserCard(userDetailCards[index]);
-        }
-    } catch (error) {
-        console.error(error);
+    for (let index = 0; index < userDetailCards.length; index++) {
+        displayUserCard(userDetailCards[index]);
     }
-
 }
 
 function resetUserCardsWindow() {
     userCardsHtml.innerHTML = "";
 }
 
-async function displayUserCard(userDetailCard) {
-    try {
-        console.log("displayUserCard userDetailCard");
-        console.log(userDetailCard);
-
-        userCardsHtml.appendChild(userDetailCard);
-    } catch (error) {
-        console.error(error);
-    }
+function displayUserCard(userDetailCard) {
+    userCardsHtml.appendChild(userDetailCard);
 }
 
 function createUserDetailCards(userList) {
-    try {
-        let userDetailCards = userList.map((element) => createUserDetailCard(element));
-        console.log("User details cards all:" + userDetailCards);
-        return userDetailCards;
-    } catch (error) {
-        console.error(error);
-    }
+    let userDetailCards = userList.map((element) => createUserDetailCard(element));
+    return userDetailCards;
 }
 
 function createUserDetailCard(user) {
-    try {
-        console.log("createUserDetailCard --START");
-        console.log(user);
+    let cardDiv = document.createElement('div');
+    cardDiv.classList.add("userCard");
 
-        let cardDiv = document.createElement('div');
-        cardDiv.classList.add("userCard");
+    let userNamDiv = document.createElement('div');
+    userNamDiv.classList.add("userName");
+    userNamDiv.textContent = user.name;
 
-        let userNamDiv = document.createElement('div');
-        userNamDiv.classList.add("userName");
-        userNamDiv.textContent = user.name;
+    let userEmailDiv = document.createElement('div');
+    userEmailDiv.classList.add("userEmail");
+    userEmailDiv.textContent = user.email;
 
-        let userEmailDiv = document.createElement('div');
-        userEmailDiv.classList.add("userEmail");
-        userEmailDiv.textContent = user.email;
+    let userCompanyDiv = document.createElement('div');
+    userCompanyDiv.classList.add("userCompany");
+    userCompanyDiv.textContent = user.company.name;
 
-        let userCompanyDiv = document.createElement('div');
-        userCompanyDiv.classList.add("userCompany");
-        userCompanyDiv.textContent = user.company.name;
+    cardDiv.appendChild(userNamDiv);
+    cardDiv.appendChild(userEmailDiv);
+    cardDiv.appendChild(userCompanyDiv);
 
-        cardDiv.appendChild(userNamDiv);
-        cardDiv.appendChild(userEmailDiv);
-        cardDiv.appendChild(userCompanyDiv);
-
-        console.log(cardDiv.outerHTML);
-
-        console.log("createUserDetailCard --END");
-
-        return cardDiv;
-
-    } catch (error) {
-        console.error(error);
-        return null;
-    }
+    return cardDiv;
 }
 
 async function main() {
